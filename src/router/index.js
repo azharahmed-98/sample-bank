8ac4bf15cd4d2ee69343a8863bf2522d2d86f82f
Azhar Ahmed A <azhar.a.ahmed98@gmail.com>
2021-09-02 21:06:45 +0530

    Adding View routes and creating the View Files.

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,34 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import Home from '../views/Home.vue';
+import Dashboard from '../views/Dashboard.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home,
+    name: 'Dashboard',
+    component: Dashboard,
   },
   {
     path: '/about',
     name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    component: () => import('../views/About.vue'),
+  },
+  {
+    path: '/products',
+    name: 'Products',
+    component: () => import('../views/Products.vue'),
+  },
+  {
+    path: '/newsRooms',
+    name: 'News Rooms',
+    component: () => import('../views/Newsrooms.vue'),
+  },
+  {
+    path: '/contactUs',
+    name: 'Contact Us',
+    component: () => import('../views/ContactUs.vue'),
   },
 ];
 
